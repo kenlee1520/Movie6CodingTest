6377236b0d354f9f4dce12c57efb2f67da109dbe
ken1520 <1155079729@link.cuhk.edu.hk>
2019-02-13 14:05:23 +0800

    component added

---
+++ src/components/MovieListButton.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import colors from '../styles/colors'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native'
+import RatingBadge from './RatingBadge'
+import IconCounter from './IconCounter'
+
+export default class MovieListButton extends Component {
+  render () {
+    const {
+      thumbnail,
+      ratingValue,
+      movieName,
+      favCount,
+      commentCount,
+      openDate
+    } = this.props
+    return (
+      <View style={styles.wrapper}>
+        <View style={styles.thumbnailContainer}>
+          <Image
+            source={{ uri: { thumbnail } }}
+          />
+        </View>
+        <View style={styles.ratingContainer}>
+          <RatingBadge
+            ratingValue={ratingValue}
+          />
+        </View>
+        <View style={styles.movieContainer}>
+          <Text style={styles.movieName}>{movieName}</Text>
+          <IconCounter
+            icon={'heart-o'}
+            count={favCount}
+          />
+          <IconCounter
+            icon={'comment-o'}
+            count={commentCount}
+          />
+          <Text style={styles.openDate}>{openDate}</Text>
+        </View>
+      </View>
+    )
+  }
+}
+
+MovieListButton.propTypes = {
+  thumbnail: PropTypes.string,
+  ratingValue: PropTypes.number,
+  movieName: PropTypes.string.isRequired,
+  favCount: PropTypes.number,
+  commentCount: PropTypes.number,
+  openDate: PropTypes.string
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: colors.black
+  },
+  thumbnailContainer: {
+    flex: 1
+  },
+  ratingContainer: {
+    flex: 1
+  },
+  movieContainer: {
+    flex: 3
+  },
+  movieName: {
+    color: colors.white
+  },
+  openDate: {
+    color: colors.white
+  }
+})
